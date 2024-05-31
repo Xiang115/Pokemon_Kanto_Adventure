beb991660ffbbb31d2cd3305416d3785f9134f60
Xiang115 <gohkimxiang0115goh@gmail.com>
2024-05-31 19:39:22 +0800

    :sparkles: Change the folder

--- src/main/java/pokemon_kanto_adventure/City.java
+++ src/main/java/pokemon_kanto_adventure/City.java
@@ -1,17 +1,19 @@
 package pokemon_kanto_adventure;
 
-public class City<T extends Comparable<T>,N extends Comparable<N>> { //City class for all the cities in the region
+public class City<T extends Comparable<T>, N extends Comparable<N>> { //City class for all the cities in the region
     T cityInfo; //cityInfo, which is the name of the city
     int deg; //degree of the city
-    City<T,N>nextCity; //pointer to next city
-    Edge<T,N>firstEdge; //pointer to first edge
-    public City(){ //create city object
+    City<T, N> nextCity; //pointer to next city
+    Edge<T, N> firstEdge; //pointer to first edge
+
+    public City() { //create city object
         cityInfo = null;
         deg = 0;
         nextCity = null;
         firstEdge = null;
     }
-    public City(T vInfo,City<T,N>next){ //create city object with pointer to next city
+
+    public City(T vInfo, City<T, N> next) { //create city object with pointer to next city
         cityInfo = vInfo;
         deg = 0;
         nextCity = next;
--- src/main/java/pokemon_kanto_adventure/PokeMaze.java
+++ src/main/java/pokemon_kanto_adventure/PokeMaze.java
@@ -5,34 +5,35 @@ import java.util.Scanner;
 
 public class PokeMaze {
     // row = 9, cloumn = 17
-    private static char[][] graph = {
-            { '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#' },
-            { '#', 'S', '.', '.', '.', '.', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#' },
-            { '#', '#', '#', '#', '#', '.', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#' },
-            { '#', '.', '.', '.', '#', '.', '#', '#', '#', '.', '.', '.', '.', '.', '#', '#', '#' },
-            { '#', '#', '#', '.', '#', '.', '#', '#', '#', 'G', '#', '.', '#', '.', '#', '#', '#' },
-            { '#', '.', '.', '.', '#', '.', '.', '.', '#', '.', '.', '.', '#', '.', '.', '.', '#' },
-            { '#', '#', '#', '#', '#', 'G', '#', '.', '#', '.', '#', '.', '#', '#', '#', '.', '#' },
-            { '#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#', '.', '.', '.', '.', '.', '#' },
-            { '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', 'E', '#' },
+    private static char[][] graph = { //the maze pattern
+            {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
+            {'#', 'S', '.', '.', '.', '.', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
+            {'#', '#', '#', '#', '#', '.', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
+            {'#', '.', '.', '.', '#', '.', '#', '#', '#', '.', '.', '.', '.', '.', '#', '#', '#'},
+            {'#', '#', '#', '.', '#', '.', '#', '#', '#', 'G', '#', '.', '#', '.', '#', '#', '#'},
+            {'#', '.', '.', '.', '#', '.', '.', '.', '#', '.', '.', '.', '#', '.', '.', '.', '#'},
+            {'#', '#', '#', '#', '#', 'G', '#', '.', '#', '.', '#', '.', '#', '#', '#', '.', '#'},
+            {'#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#', '.', '.', '.', '.', '.', '#'},
+            {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', 'E', '#'},
     };
-    private static Stack<Integer> row;
-    private static Stack<Integer> col;
-    private static Stack<String> movement;
+    private static Stack<Integer> row;//stack containing passed row index for bactracking
+    private static Stack<Integer> col;//stack containing passed column index for backtracking
+    //row + col at same index together represent the position history
+    private static Stack<String> movement; //stack conatining movement history for backtracking
 
     public PokeMaze() {
-        row = new Stack<>();
+        row = new Stack<>(); //create new Stacks
         col = new Stack<>();
         movement = new Stack<>();
-        System.out.println("Welcome to the PokeMaze Challenge");
+        System.out.println("Welcome to the PokeMaze Challenge"); //display welcome to the maze message
         System.out.println("Find your way through the maze using stacks");
-        System.out.println("Legend: # - Wall, . - Path, S - Start, E - End, G - Ghastly");
-        row.push(1);
+        System.out.println("Legend: # - Wall, . - Path, S - Start, E - End, G - Ghastly");//display maze legend
+        row.push(1);//push the initial position
         col.push(1);
         InitializeGraph();
     }
 
-    public void InitializeGraph() {
+    public void InitializeGraph() { //Initialize the Maze and draw it using the maze pattern above
         for (char[] Gcol : graph) {
             for (char Grow : Gcol) {
                 System.out.print(Grow + "  ");
@@ -41,7 +42,7 @@ public class PokeMaze {
         }
     }
 
-    public void PrintGraph() {
+    public void PrintGraph() { //print the maze and the player's position everytime player makes a move
         for (int i = 0; i < graph.length; i++) {
             for (int j = 0; j < graph[i].length; j++) {
                 char elem = (row.peek().compareTo(i) == 0 && col.peek().compareTo(j) == 0) ? 'Y' : graph[i][j];
@@ -51,39 +52,39 @@ public class PokeMaze {
         }
     }
 
-    public void simulation(Player player) {
+    public void simulation(Player player) { //method that allows player walk through the maze
         Scanner sc = new Scanner(System.in);
-        while (true) {
-            boolean G = false, E = false, W = false;
-            System.out.print("Enter direction (up, down, left, right): ");
-            String choice = sc.nextLine();
-            move(choice);
-            char pointer = graph[row.peek()][col.peek()];
-            if (pointer == 'G') {
-                G = true;
-                PrintGraph();
-                System.out.println("Oh no! You encountered a Ghastly and got caught.");
-                System.out.println("Game Over.");
-                break;
-            } else if (pointer == 'E') {
-                E = true;
-                PrintGraph();
-                System.out.println("Congratulations! You've reached the end of the maze.");
+        while (true) { //while player does not get caught by Ghastly or reach the end of the maze the loop will not end
+            boolean G = false, E = false, W = false;//G is the state of getting caught by Ghastly, E is the state of reaching the end of the maze, W is the state of bumping into a wall
+            System.out.print("Enter direction (up, down, left, right): ");//get direction from user
+            String choice = sc.nextLine();//receive direction
+            move(choice);//move the player according to the direction
+            char pointer = graph[row.peek()][col.peek()];//check the player's position
+            if (pointer == 'G') {//if the player's position is on a Ghastly
+                G = true; //player gets caught by Ghastly
+                PrintGraph();//display the maze and player's position
+                System.out.println("Oh no! You encountered a Ghastly and got caught.");//display get caught message
+                System.out.println("Game Over.");//display game over message
+                break;//end loop
+            } else if (pointer == 'E') {//if player's position is the end of hte maze
+                E = true;//player reaches the end of the maze
+                PrintGraph();//display the maze and the player's position
+                System.out.println("Congratulations! You've reached the end of the maze.");//display win message
                 System.out.println("You found $1000 in the maze!");
-                player.addMoney(1000);
-                break;
-            } else if (pointer == '#') {
-                W = true;
-                System.out.println("Invalid move.");
-                backmove(choice);
-                PrintGraph();
+                player.addMoney(1000);//player earns $1000 as reward
+                break;//end loop
+            } else if (pointer == '#') {//if player's position is on a wall
+                W = true;//player bumps into a wall
+                System.out.println("Invalid move.");//display invalid move message
+                backmove(choice);//let player go back one step, before bumping on the wall
+                PrintGraph();//display maze and player position
             }
-            if (!G && !E && !W) {
-                PrintGraph();
+            if (!G && !E && !W) {//if neither of the three mentioned states is true, that means player is on a path, continue the maze game
+                PrintGraph();//print the maze and the player's loaction
             }
         }
-        System.out.println("Total steps: " + movement.size());
-        System.out.println("Your path:");
+        System.out.println("Total steps: " + movement.size());//if maze is ended, no matter win or lost, display the total steps
+        System.out.println("Your path:");//backtrack the path taken and print it out
         StringBuilder sb = new StringBuilder();
         while (!movement.isEmpty()) {
             sb.insert(0, (" --> " + movement.pop() + " --> | " + col.pop() + " , " + row.pop() + " |"));
@@ -92,38 +93,38 @@ public class PokeMaze {
         System.out.println(sb);
     }
 
-    public void move(String choice) {
+    public void move(String choice) {//move the player according to movement
 
         switch (choice) {
-            case "right":
-                movement.push(choice);
-                col.push(col.peek() + 1);
+            case "right"://if player moves right
+                movement.push(choice);//push the movement into movement history stack
+                col.push(col.peek() + 1);//push the new coordinates of the player into col and row, col+1 because moving right
                 row.push(row.peek());
                 break;
-            case "left":
-                movement.push(choice);
-                col.push(col.peek() - 1);
+            case "left"://if player moves left
+                movement.push(choice);//push the movement into movement history stack
+                col.push(col.peek() - 1);//push the new coordinates of the player into col and row, col-1 because moving right
                 row.push(row.peek());
                 break;
-            case "up":
-                movement.push(choice);
-                row.push(row.peek() - 1);
+            case "up"://if the player moves up
+                movement.push(choice);//push the movement into movement history stack
+                row.push(row.peek() - 1);//push the new coordinates of the player into col and row, row-1 because moving up
                 col.push(col.peek());
                 break;
-            case "down":
-                movement.push(choice);
-                row.push(row.peek() + 1);
+            case "down"://if the player moves down
+                movement.push(choice);//push the movement into movement history stack
+                row.push(row.peek() + 1);//push the new coordinates of the player into col and row, row+1 because moving down
                 col.push(col.peek());
                 break;
             default:
-                System.out.println("Inavailable choice");
+                System.out.println("Inavailable choice");//if invalid direction print this
         }
     }
 
-    public void backmove(String choice) {
+    public void backmove(String choice) {//move back to last coordinates
 
         switch (choice) {
-            case "right":
+            case "right"://all cases are the same, pop top element in movement, row, col
                 movement.pop();
                 col.pop();
                 row.pop();
@@ -143,7 +144,7 @@ public class PokeMaze {
                 row.pop();
                 col.pop();
                 break;
-            default:
+            default://if direction choice is invalid, print message below
                 System.out.println("Inavailable choice");
         }
     }
